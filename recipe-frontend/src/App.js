2f27276dd5c06bad031d83563a83117a21a03bc3
Michael Calabrese <michaelcalabrese@unknown6ccdd6254a59.attlocal.net>
2024-08-20 15:22:53 -0500

    cleaned up a bit with bootstrap

--- recipe-frontend/src/App.js
+++ recipe-frontend/src/App.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import RecipeList from './components/RecipeList';
 import RecipeDetail from './components/RecipeDetail';
-import RecipeForm from './components/RecipeForm';
+import RecipeForm from './components/RecipeForm'
+import { Container, Navbar, Nav, Row, Col } from 'react-bootstrap';
 
 function App() {
   const [selectedRecipeId, setSelectedRecipeId] = useState(null);
@@ -13,13 +14,34 @@ function App() {
 
   return (
     <div>
-      <h1>Recipe Manager</h1>
-      <RecipeForm recipeId={selectedRecipeId} onSubmitSuccess={handleSuccess} />
-      <RecipeList 
-        refresh={refresh}
-        onRecipeSelect={setSelectedRecipeId} 
-      />
-      <RecipeDetail recipeId={selectedRecipeId} />
+      <Navbar bg="dark" variant="dark" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">Recipe Manager</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#recipes">Recipes</Nav.Link>
+              <Nav.Link href="#about">About</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      <Container className="mt-4">
+        <Row>
+          <Col md={4}>
+            <RecipeForm recipeId={selectedRecipeId} onSubmitSuccess={handleSuccess} />
+          </Col>
+          <Col md={8}>
+            <RecipeList 
+              refresh={refresh}
+              onRecipeSelect={setSelectedRecipeId} 
+            />
+            {selectedRecipeId && <RecipeDetail recipeId={selectedRecipeId} />}
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
